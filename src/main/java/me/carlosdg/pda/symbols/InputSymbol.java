b6d4121118a629971a0f4d47548d4c1f66c57970
Carlos Domínguez García <carlosdg97@gmail.com>
2018-10-22 22:33:51 +0100

    Feat: equals and hashcode to Symbol subclasses
    Need these methods to use instances as element of collections. Also removed the InputSymbol#from static method used to create an optional string (empty optional representing empty string) because we don't have use for that at the moment

--- src/main/java/me/carlosdg/pda/symbols/InputSymbol.java
+++ src/main/java/me/carlosdg/pda/symbols/InputSymbol.java
@@ -1,31 +1,33 @@
 package me.carlosdg.pda.symbols;
 
-import java.util.Optional;
-
 /**
  * Class to represent input symbols
  *
  * @author Carlos Domínguez García
  */
 public class InputSymbol extends Symbol {
-	/**
-	 * Returns an empty optional if the given string is the representation of the
-	 * empty string. Otherwise returns an optional with the symbol object
-	 *
-	 * @param representation String to construct the symbol from
-	 * @return An optional with the symbol object or empty if the given
-	 *         representation is the one of the empty string
-	 */
-	public static Optional<Symbol> from(String representation) {
-		if (representation.equals(Symbol.EMPTY_STRING_REPR)) {
-			return Optional.empty();
-		}
-
-		return Optional.of(new InputSymbol(representation));
-	}
-
 	/** @see me.carlosdg.pda.symbols.Symbol */
 	public InputSymbol(String representation) {
 		super(representation);
+	}
+
+	@Override
+	public int hashCode() {
+		return 31 * getClass().hashCode() + getRepresentation().hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
+		if (this == obj) {
+			return true;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+
+		return ((InputSymbol) obj).getRepresentation().equals(getRepresentation());
 	}
 }
--- src/main/java/me/carlosdg/pda/symbols/StackSymbol.java
+++ src/main/java/me/carlosdg/pda/symbols/StackSymbol.java
@@ -10,4 +10,24 @@ public class StackSymbol extends Symbol {
 	public StackSymbol(String representation) {
 		super(representation);
 	}
+
+	@Override
+	public int hashCode() {
+		return 31 * getClass().hashCode() + getRepresentation().hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
+		if (this == obj) {
+			return true;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+
+		return ((StackSymbol) obj).getRepresentation().equals(getRepresentation());
+	}
 }
