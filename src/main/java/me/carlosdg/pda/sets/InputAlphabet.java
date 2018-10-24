75b1fa0baa06c6451a3c62d1bf15043cc94b4812
Carlos Domínguez García <carlosdg97@gmail.com>
2018-10-24 17:16:27 +0100

    Fix: alphabet sets specialized
    The alphabet sets where inheriting from SymbolSet<AlphabetSymbol> instead of their corresponding Symbol class as the generic argument

--- src/main/java/me/carlosdg/pda/sets/InputAlphabet.java
+++ src/main/java/me/carlosdg/pda/sets/InputAlphabet.java
@@ -4,21 +4,20 @@ import java.util.Collection;
 
 import me.carlosdg.pda.symbols.InputAlphabetSymbol;
 import me.carlosdg.pda.sets.exceptions.DuplicatedStringInSetException;
-import me.carlosdg.pda.symbols.AlphabetSymbol;
 
 /**
  * Input alphabet class
  *
  * @author Carlos Domínguez García
  */
-public class InputAlphabet extends SymbolSet<AlphabetSymbol> {
+public class InputAlphabet extends SymbolSet<InputAlphabetSymbol> {
 
 	public InputAlphabet(Collection<String> symbolRepresentations) throws DuplicatedStringInSetException {
 		super(symbolRepresentations);
 	}
 
 	@Override
-	protected AlphabetSymbol newSymbol(String representation) {
+	protected InputAlphabetSymbol newSymbol(String representation) {
 		return new InputAlphabetSymbol(representation);
 	}
 
--- src/main/java/me/carlosdg/pda/sets/StackAlphabet.java
+++ src/main/java/me/carlosdg/pda/sets/StackAlphabet.java
@@ -4,21 +4,20 @@ import java.util.Collection;
 
 import me.carlosdg.pda.symbols.StackAlphabetSymbol;
 import me.carlosdg.pda.sets.exceptions.DuplicatedStringInSetException;
-import me.carlosdg.pda.symbols.AlphabetSymbol;
 
 /**
  * Stack alphabet class
  *
  * @author Carlos Domínguez García
  */
-public class StackAlphabet extends SymbolSet<AlphabetSymbol> {
+public class StackAlphabet extends SymbolSet<StackAlphabetSymbol> {
 
 	public StackAlphabet(Collection<String> symbolRepresentations) throws DuplicatedStringInSetException {
 		super(symbolRepresentations);
 	}
 
 	@Override
-	protected AlphabetSymbol newSymbol(String representation) {
+	protected StackAlphabetSymbol newSymbol(String representation) {
 		return new StackAlphabetSymbol(representation);
 	}
 
